d3e84997058448c7dc7d7dd27069b8bc5f0d4fb9
tomwray13 <tomwray13@gmail.com>
2023-11-29 09:13:05 +0000

    Fix bug - include FileService in exports

--- src/utilities/utilities.module.ts
+++ src/utilities/utilities.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { FileService } from './file/file.service';
 
 @Module({
-  providers: [FileService]
+  providers: [FileService],
+  exports: [FileService],
 })
 export class UtilitiesModule {}
